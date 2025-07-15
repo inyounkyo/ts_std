e6cad2aca03eff404a9f669d310e725bf076852c
inyounkyo <inyounkyo@hanmail.net>
2025-07-15 13:29:50 +0900

    first ts script

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HelloTs from "./components/HelloTs";
+
+function App() {
+  return (
+    <div className="App">
+      <HelloTs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloTs.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloTs = () => {
+  return <div>Hello Typescript !!!````</div>;
+};
+
+export default HelloTs;
