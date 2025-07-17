b3aea6ef4500fcc1845e7509af8a17cee8de78b3
younkyo <inyounkyo@hanmail.net>
2025-07-17 17:04:11 +0900

    ts_std_3day

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import "./App.css";
 import EverydayTypes from "./components/EverydayTypes";
 import Narrowing from "./components/Narrowing";
 import MoreonFunctions from "./components/MoreonFunctions";
+import Intro from './components/intro_01/Intro';
+import Myfuntions from "./components/basics_02/Myfuntions";
 
 function App() {
   return (
     <div className="App">
       {/* <EverydayTypes /> */}
       {/* <Narrowing /> */}
-      {<MoreonFunctions />}
+      {/* {<MoreonFunctions />} */}
+      {/* <Intro /> */}
+      <Myfuntions />
     </div>
   );
 }
